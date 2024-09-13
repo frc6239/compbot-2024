325c4a9c364e31e86ca22c35c7d3a09d631ad25e
Jude Johnson <80632047+judejhnsn@users.noreply.github.com>
2024-09-12 20:46:32 -0400

    Added the indexerSubsystem.java, Modifyed intakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -4,14 +4,45 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
 
 public class IndexerSubsystem extends SubsystemBase {
+  private final CANSparkMax indexerMotor;
+  private boolean enabled;
+  
   /** Creates a new IndexerSubsystem. */
-  public IndexerSubsystem() {}
+  public IndexerSubsystem() {
+      indexerMotor = new CANSparkMax(22, MotorType.kBrushless);
+      enabled = false;
+  }
+  public void run() {
+    enabled = true;
+  }
+
+  public void stop() {
+    indexerMotor.set(0);
+    enabled = false;
+  }
+
+  public void invert() {
+    indexerMotor.setInverted(!indexerMotor.getInverted());
+  }
+
+  public boolean getIsRunning() {
+    return enabled;
+  }
 
   @Override
   public void periodic() {
+    if(enabled){
+    indexerMotor.set(IntakeConstants.MAX_INTAKE_SPEED);
+    } else {
+      indexerMotor.set(0);
+    }
     // This method will be called once per scheduler run
   }
 }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -12,30 +12,26 @@ import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
   private final CANSparkMax intakeMotor;
-  private final CANSparkMax indexerMotor;
   private boolean isRunning;
 
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem() {
     intakeMotor = new CANSparkMax(21, MotorType.kBrushless);
-    indexerMotor = new CANSparkMax(22, MotorType.kBrushless);
+  
   }
 
   public void run() {
     intakeMotor.set(IntakeConstants.MAX_INTAKE_SPEED);
-    indexerMotor.set(IntakeConstants.MAX_INDEXER_SPEED);
     isRunning = true;
   }
 
   public void stop() {
     intakeMotor.set(0);
-    indexerMotor.set(0);
     isRunning = false;
   }
 
   public void invert() {
     intakeMotor.setInverted(!intakeMotor.getInverted());
-    indexerMotor.setInverted(!intakeMotor.getInverted());
   }
 
   public boolean getIsRunning() {
@@ -44,6 +40,7 @@ public class IntakeSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    run();
     // This method will be called once per scheduler run
   }
 }
